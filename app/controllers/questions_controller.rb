ded1cb5cbffd1814f637229f6ce4e095eee53ae5
Nadia Salmen <nadiasalmen@gmail.com>
2021-01-01 20:44:05 -0300

    Add user story - coach answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,18 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    a = 'Great!'
+    b = 'Silly question, get dressed and go to work!'
+    c = "I don't care, get dressed and go to work!"
+    @question = params[:question]
+    if @question == 'I am going to work right now!'
+      @answer = a
+    elsif @question.end_with?('?')
+      @answer = b
+    else
+      @answer = c
+    end
+  end
 end
